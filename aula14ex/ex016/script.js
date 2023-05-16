7a10e18463198219696ecb410ac4adad9cd5d9cb
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-05-16 18:21:46 -0300

    atualizado ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,8 +1,20 @@
-function contar()
-    var c = 1
+function contar() {
+    var ini = document.getElementById('iinicio')
+    var fim = document.getElementById('ifim')
+    var passo = document.getElementById('ipasso')
     var res = window.document.querySelector('div#res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        alert('TUDO OK!')
+    }
+    
+    /*
     while (c <= 10) {
         window.write(c)
         c++
     }
     res.innerHTML = `Contando: ${c}`
+    */
+}
