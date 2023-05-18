fe5cd4d9126d199e7eb56fdd18701072203a7cb0
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-05-18 11:36:47 -0300

    atualizado script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,18 +3,32 @@ function contar() {
     var fim = document.getElementById('ifim')
     var passo = document.getElementById('ipasso')
     var res = window.document.querySelector('div#res')
+    
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        // window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
     } else {
-        alert('TUDO OK!')
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO = 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+           for(var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            } 
+        } else {
+            // Contagem regressiva
+            for(var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
     
-    /*
-    while (c <= 10) {
-        window.write(c)
-        c++
-    }
-    res.innerHTML = `Contando: ${c}`
-    */
 }
