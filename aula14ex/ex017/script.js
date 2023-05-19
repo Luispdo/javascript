023cb19de470b3c84dcef092ac9e69fad6a9d21a
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-05-19 17:27:47 -0300

    atualizado o script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,4 +1,27 @@
-function gerar() {
+function calcular() {
     var num = document.getElementById('inum')
-    var n = Number(num.value)
+    var tab = document.getElementById('itabuada')
+    
+    if (num.value.length == 0) {
+        alert('[ERRO] Insira um número para continuar!')
+    } else {
+        var n = Number(num.value)
+        // var c = 1
+        tab.innerHTML = ''
+        for (c = 1; c <= 10; c++) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+        }
+        /*while (c <= 10) {
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+        */
+
+    }
+
+   
 }
