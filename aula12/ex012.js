d6bebdb48fb3b9693759b8e36d87b9584a10d089
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-05-23 14:49:02 -0300

    atualizado ex012.js

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,10 +1,12 @@
 var agora = new Date()
 var hora = agora.getHours()
-// var hora = 13
+// var hora = 2
 console.log(`Agora são exatamente ${hora} horas.`)
-if (hora < 12) {
-    console.log('Bom Dia!')
-} else if (hora <=18) {
+if (hora >= 0 && hora < 6) {
+    console.log('Boa Madrugada!')
+} else if (hora < 12) {
+    console.log('Boa Dia!')
+} else if (hora < 18) {
     console.log('Boa Tarde!')
 } else {
     console.log('Boa Noite!')
