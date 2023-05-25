e30538bd4e1c61e6f1c0ab20ce0cced258974c87
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-05-25 16:12:23 -0300

    atualizado index.html

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,1 +1,13 @@
-alert('Olá!')
+function adicionar() {
+    let num = document.getElementById('inum')
+
+    if(num.value.length == 0) {
+        alert('[ERRO] Valor inválido ou já encontrado na lista.')
+    }
+
+}
+
+
+function finalizar() {
+
+}
