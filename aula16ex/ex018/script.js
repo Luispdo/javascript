bd4671b6cb941676595b4216630e0bf54bb39dac
Luís Paulo de Oliveira <129074831+Luispdo@users.noreply.github.com>
2023-05-26 17:21:45 -0300

    atualizado script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -22,12 +22,11 @@ function inLista(n, l) {
 function adicionar() {
     
     if(isNumero(num.value) && !inLista(num.value, valores)) {
-        //lista.innerHTML = ''
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
-
+        res.innerHTML = ''
     } else {
         alert('[ERRO] Valor inválido ou já encontrado na lista.')
     }
@@ -35,6 +34,35 @@ function adicionar() {
     num.focus()
 }
 
+function contar() {
+
+}
+
 function finalizar() {
+    if(valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let cont = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        for(let pos in valores) {
+            if (valores[pos] > maior)
+        }
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${cont} números cadastrados.</p>`
+    }
+    
+    /*
+    let cont = 
+
+    cont.innerHTML = 
+    maior.innerHTML = `O maior valor informado foi ${maior}.`
+    menor.innerHTML = `O menor valor informado foi ${menor}.`
+    soma.innerHTML = `Somando todos os valores, temos ${soma}.`
+    media.innerHTML = `A média dos valores digitados é ${media}.`
+*/
+
+
 
 }
